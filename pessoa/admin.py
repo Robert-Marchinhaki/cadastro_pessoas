40e716f370e36afc751937a65f62c0a50d9ce1af
Robert Adrian <adrianrobert288@gmail.com>
2022-06-24 15:16:08 -0300

    Criado painel do django admin
    	- criado filtros
    	- cirado funções para ativar/desativar vários usuários
    	  simultaneamente

--- pessoa/admin.py
+++ pessoa/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Contato, Pessoas
 
 # Register your models here.
+
+
+@admin.action(description="Ativar todas as pessoas")
+def ativar_todos(modeladmin, request, queryset):
+    queryset.update(ativo=True)
+
+
+@admin.action(description='Desativar todas as pessoas')
+def desativar_todos(modeladmin, request, queryset):
+    queryset.update(ativo=False)
+
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome_completo',
+        'data_nascimento',
+        'ativo'
+    ]
+    list_filter = [
+        'ativo',
+        'data_nascimento'
+    ]
+    search_fields = [
+        'nome_completo'
+    ]
+    actions = [
+        ativar_todos,
+        desativar_todos
+    ]
+
+
+admin.site.register(Pessoas, PessoaAdmin)
+admin.site.register(Contato)
